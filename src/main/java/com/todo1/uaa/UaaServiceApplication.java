a37322b85fb165f51e4bbb698814e59eeec193c4
Junior <angel_96vir@hotmail.com>
2021-05-31 19:05:01 -0500

    first commit develop

---
+++ src/main/java/com/todo1/uaa/UaaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.todo1.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaaServiceApplication.class, args);
+	}
+
+}
